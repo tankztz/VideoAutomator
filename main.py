7197ca157089a66dee6eb4999379b13f42937105
tankztz <tankztz@gmail.com>
2020-06-22 19:17:05 +0800

    Add list command, change output file name

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import os
-from src import tts
+from src import google_tts as tts
 
-print("using credential: " + os.environ['GOOGLE_APPLICATION_CREDENTIALS'])
+if os.environ['GOOGLE_APPLICATION_CREDENTIALS']:
+    print("using credential: " + os.environ['GOOGLE_APPLICATION_CREDENTIALS'])
+else:
+    print("No google cloud credential specified, cannot reach google api.")
 
 
 def main():
-    mp3file = tts.get_mp3("所以才想着来找你问问")
+    mp3file = tts.get_mp3(
+        "白皮书说，党的十八大以来，中国的核安全事业进入安全高效发展的新时期。在核安全观引领下，中国逐步构建起法律规范、行政监管、行业自律、技术保障、人才支撑、文化引领、社会参与、国际合作等为主体的核安全治理体系，核安全防线更加牢固。",
+        name="cmn-CN-Wavenet-B"
+    )
     os.system("start " + mp3file)
+    # tts.list_voices("zh-CN")
 
 
 if __name__ == "__main__":
--- src/google_tts.py
+++ src/google_tts.py
@@ -1,11 +1,39 @@
+import datetime
 import random
 from google.cloud import texttospeech
+
+
+def list_voices(language_code=None):
+    """Lists the available voices."""
+    from google.cloud import texttospeech
+
+    client = texttospeech.TextToSpeechClient()
+
+    # Performs the list voices request
+    voices = client.list_voices(language_code=language_code)
+
+    for voice in voices.voices:
+        # Display the voice's name. Example: tpc-vocoded
+        print(f"Name: {voice.name}")
+
+        # Display the supported language codes for this voice. Example: "en-US"
+        for language_code in voice.language_codes:
+            print(f"Supported language: {language_code}")
+
+        ssml_gender = texttospeech.SsmlVoiceGender(voice.ssml_gender)
+
+        # Display the SSML Voice Gender
+        print(f"SSML Voice Gender: {ssml_gender.name}")
+
+        # Display the natural sample rate hertz for this voice. Example: 24000
+        print(f"Natural Sample Rate Hertz: {voice.natural_sample_rate_hertz}\n")
 
 
 def get_mp3(
         text,
         language_code="zh-CN",
-        audio_encoding=texttospeech.AudioEncoding.MP3
+        audio_encoding=texttospeech.AudioEncoding.MP3,
+        name="cmn-CN-Wavenet-A"
 ):
     """Synthesizes speech from the input string of text or ssml.
     Note: ssml must be well-formed according to:
@@ -20,7 +48,7 @@ def get_mp3(
     # Build the voice request, select the language code ("en-US") and the ssml
     # voice gender ("neutral")
     voice = texttospeech.VoiceSelectionParams(
-        language_code=language_code, ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+        language_code=language_code, ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL, name=name
     )
 
     # Select the type of audio file you want returned
@@ -34,7 +62,7 @@ def get_mp3(
         input=synthesis_input, voice=voice, audio_config=audio_config
     )
 
-    output_file_name = "..\\output\\" + "output" + str(random.randint(0, 100)) + ".mp3"
+    output_file_name = "..\\output\\" + "output" + datetime.datetime.now().strftime("%Y%m%d%H%M%S") + ".mp3"
     # The response's audio_content is binary.
     with open(output_file_name, "wb") as out:
         # Write the response to the output file.
